7d56090e69998524833b954a6b9505622a23f64f
saleemLawal <slawal1@umbc.edu>
2025-03-18 20:39:31 -0400

    added layout design for desktop and mobile navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const interSans = Inter({
   variable: "--font-inter-sans",
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${interSans.variable} antialiased`}>{children}</body>
+      <body className={`${interSans.variable} antialiased`}>
+        <Navbar />
+        {children}
+        </body>
     </html>
   );
 }
